f66c87c0c1bbfa7012b74a0fcf33a3e5349b5809
Aleks <korparativ@mail.ru>
2024-02-23 17:47:50 +1200

    Подсчет размера для создания нового массива

--- Program.cs
+++ Program.cs
@@ -5,3 +5,12 @@
         Console.Write(e + " ");
     }
 }
+
+string[] array = { "Hello", "2", "world", ":-)" };
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        j++;
+}
+string[] arr = new string[j];
